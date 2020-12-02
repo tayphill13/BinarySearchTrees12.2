ed5427c694d80085a505b52fe09134b326e9f33c
Taylor Phillips <taylorphillips133@gmail.com>
2020-12-02 15:04:30 -0800

    binary tree searching

---
+++ __tests__/bst.test.js
@@ -0,0 +1,37 @@
+import BST from '../src/bst.js';
+import BSTNode from '../src/bst-node.js';
+
+describe('binarySearchTree', () => {
+
+  const pbst = new BST();
+
+  beforeEach(() => {
+    pbst.insert(new BSTNode(4));
+    pbst.insert(new BSTNode(2));
+    pbst.insert(new BSTNode(6));
+    pbst.insert(new BSTNode(1));
+    pbst.insert(new BSTNode(3));
+    pbst.insert(new BSTNode(5));
+    pbst.insert(new BSTNode(7));
+  });
+
+  test('it should return true if the root node is equal to 4', () => {
+    expect(pbst.search(4)).toEqual(true);
+  })
+
+  test('it should return false if the root node is not equal to 31', () => {
+      expect(pbst.search(31)).toEqual(false);
+  });
+
+  test('it should return true if the first child node on the left is equal to 2', () => {
+    expect(pbst.search(2)).toEqual(true);
+  });
+
+  test('it should return true if the tree includes 7', () => {
+    expect(pbst.search(7)).toEqual(true);
+  });
+
+  test('it should return true if the tree includes 7', () => {
+    expect(pbst.search(5)).toEqual(true);
+  });
+});
---
+++ src/bst.js
@@ -0,0 +1,50 @@
+export default class BST {
+  constructor() {
+    this.root = null;
+  }
+  search(value) {
+    if (this.root.data === value) {
+      return true;
+    } else {
+      let currentNode = this.root;
+      while (true) {
+        if (currentNode.data === value) {
+          return true;
+        } else if (currentNode.data > value) {
+          currentNode = currentNode.left;
+        } else if (currentNode.data < value) {
+          currentNode = currentNode.right;
+        }
+        if (currentNode === null) {
+          return false;
+        }
+      }
+    }
+  }
+  insert(insertedNode) {       // This function was previously called insertNode because that's what it's doing.
+    if (this.root === null) {
+      this.root = insertedNode;
+    } else {
+      let currentNode = this.root;
+      while (true) {
+        if (currentNode.data > insertedNode.data) {
+          if (currentNode.left === null) {
+            currentNode.left = insertedNode;
+            return this;
+          } else {
+            currentNode = currentNode.left;
+          }
+        } else if (currentNode.data < insertedNode.data) {
+          if (currentNode.right === null) {
+            currentNode.right = insertedNode;
+            return this;
+          } else {
+            currentNode = currentNode.right;
+          }
+        } else {
+          return this;
+        }
+      }
+    }
+  }
+}
